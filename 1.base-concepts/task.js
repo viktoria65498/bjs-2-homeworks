bc1130a03a60d5f3c1c3524b6766e3527eb68836
vkkk_a <grudinovaviktoria@gmail.com>
2025-05-30 22:43:03 +0300

    base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let arr = []
+  let d = b**2 - 4 * a * c
+
+  if (d === 0) {
+    arr = [-b / (2 * a)]
+  } else if (d > 0) {
+    arr = [(- b + Math.sqrt(d) ) / (2 * a), (- b - Math.sqrt(d) ) / (2 * a)]
+  }
+  return arr
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / 12
+  let loanBody = amount - contribution
+  let monthlyPayment = loanBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)))
+  let totalAmount = monthlyPayment * countMonths
+  let rounded = Number(totalAmount.toFixed(2))
+  return rounded
 }
+
+console.log(solveEquation())
+console.log(calculateTotalMortgage())
