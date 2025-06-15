0311d92593b975b662fe0ea5e4c08f90031714d5
vkkk_a <grudinovaviktoria@gmail.com>
2025-06-16 00:33:44 +0300

    Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name
+  this.gender = gender
+  this.age = age
+  this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty("marks")) {
+    this.marks.push(...marks)
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.hasOwnProperty("marks") && (this.marks.length > 0)) {
+    let sum = 0
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i]
+    }
+    return sum / this.marks.length
+  } else {
+    return 0
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject
+  delete this.marks
+  this.excluded = reason
 }
