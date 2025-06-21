f03583a7f348cec6106650a96c5b6bffa5b43271
vkkk_a <grudinovaviktoria@gmail.com>
2025-06-22 02:05:44 +0300

    Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value) {
+    if (!isNaN(value)) {
+        return Number.parseFloat(value)
+    } else {
+        throw new Error('Невалидное значение')
+    }
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value)
+    } catch (error) {
+        return error
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a
+        this.b = b
+        this.c = c
+        if ((this.a + this.b < this.c) || (this.a + this.c < this.b) || (this.b + this.c < this.a)) {
+            throw new Error('Треугольник с такими сторонами не существует')
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c
+    }
+
+    get area() {
+        let p = 0.5 * (this.a + this.b + this.c)
+        let area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))
+        return Number(area.toFixed(3))
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c)
+    } catch (error) {
+        
+        return {
+            get area() { return ('Ошибка! Треугольник не существует') },
+            get perimeter() { return ('Ошибка! Треугольник не существует') }
+        }
+    }
+}
